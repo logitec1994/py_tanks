36f333f1b6911a47856bc73325da5c6021f15154
Alexei Larionov <123654978a@gmail.com>
2020-01-30 20:44:38 +0200

    Delete Canvas.py

--- Canvas.py
+++
@@ -1,8 +0,0 @@
-class Canvas:
-    def __init__(self, size):
-        self.size = size
-
-    def create_canvas(self):
-        canvas: list
-        canvas = ([[" " for x in range(self.size["width"])] for y in range(self.size["height"])])
-        return canvas
