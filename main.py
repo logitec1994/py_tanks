dc60e78bc110573fde416d0c4abb8cfe72092f01
Alexei Larionov <123654978a@gmail.com>
2020-03-08 14:16:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,36 +21,8 @@ from pynput import keyboard
 
 def on_press(key):
     try:
-        if key == keyboard.KeyCode.from_char("a"):
-            print("Moving Left")
-            player.create_canvas()
-            player.set_zone()
-            player.move_left()
-            player.update(player.postion['x'], player.postion['y'], player.face)
-            player.render()
-        elif key == keyboard.KeyCode.from_char("d"):
-            print("Moving Right")
-            player.create_canvas()
-            player.set_zone()
-            player.move_right()
-            player.update(player.postion['x'], player.postion['y'], player.face)
-            player.render()
-        elif key == keyboard.KeyCode.from_char("w"):
-            print("Moving Up")
-            player.create_canvas()
-            player.set_zone()
-            player.move_up()
-            player.update(player.postion['x'], player.postion['y'], player.face)
-            player.render()
-        elif key == keyboard.KeyCode.from_char("s"):
-            print("Moving Down")
-            player.create_canvas()
-            player.set_zone()
-            player.move_down()
-            player.update(player.postion['x'], player.postion['y'], player.face)
-            player.render()
-        else:
-            print("Not moving")
+        if key == 'a':
+            print("Lol! N")
     except AttributeError:
         print("No More")
 
